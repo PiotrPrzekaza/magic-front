42aaf650d410ea19a418259bfee1bfac6dbe79ad
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-11-17 19:27:07 +0100

    Initial commit. Start setup a rect project.

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import logo from "../assets/icon/jednorozec.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Magiczna Pętelka</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+
+      <main></main>
+      <footer>
+        <div>powered by pepe-dev</div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
