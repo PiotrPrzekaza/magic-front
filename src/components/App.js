b6f17155311c4973c79d3f95e9702d410bb385e2
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-11-20 17:47:18 +0100

    setup huskey and lint-staged to autofix before commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,45 @@
 import './App.css';
 import logo from '../assets/images/Magiczna.png';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <header>
-          <nav>
-            <div>
-              <img src={logo} alt="" />
-            </div>
-            <ul>
-              <li>o mnie</li>
-              <li>szydelko</li>
-              <li>druty</li>
-              <li>projecty</li>
-              <li>kontakt</li>
-            </ul>
-          </nav>
-          <h1> magia szydełka</h1>
-        </header>
-        <main>
-          <div>jakiscontent</div>
-          <div>jakiscontent</div>
-          <div>jakiscontent</div>
-          <div>jakiscontent</div>
-        </main>
-        <footer>
+const App = () => (
+  <div className="App">
+    <div className="container">
+      <header>
+        <nav>
           <div>
-            <p>sociale: </p>
-            <ul>
-              <li>Insta</li>
-              <li>Facebook</li>
-            </ul>
+            <img src={logo} alt="" />
           </div>
-          <div>
-            <p>magiczna-petelka.io</p>
-            <p>powered by Pepe-dev &copy;</p>
-          </div>
-        </footer>
-      </div>
+          <ul>
+            <li>o mnie</li>
+            <li>szydelko</li>
+            <li>druty</li>
+            <li>projecty</li>
+            <li>kontakt</li>
+          </ul>
+        </nav>
+        <h1> magia szydełka</h1>
+      </header>
+      <main>
+        <div>jakiscontent</div>
+        <div>jakiscontent</div>
+        <div>jakiscontent</div>
+        <div>jakiscontent</div>
+      </main>
+      <footer>
+        <div>
+          <p>sociale: </p>
+          <ul>
+            <li>Insta</li>
+            <li>Facebook</li>
+          </ul>
+        </div>
+        <div>
+          <p>magiczna-petelka.io</p>
+          <p>powered by Pepe-dev &copy;</p>
+        </div>
+      </footer>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
