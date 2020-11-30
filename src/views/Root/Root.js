a376ab395302da1ed46ae08ae74d53acbe3ce513
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-11-30 17:59:20 +0100

    implemented template to basic view

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -4,59 +4,54 @@ import Heading from 'components/atoms/Heading/Heading';
 import ListItem from 'components/atoms/ListItem/ListItem';
 import MenuBar from 'components/organizms/MenuBar/MenuBar';
 import ImageItem from 'components/atoms/ImageItem/ImageItem';
-import GlobalStyle from 'assets/styles/GlobalStyle';
-import theme from 'assets/styles/theme';
 import logo from 'assets/images/Magiczna.png';
-import { ThemeProvider } from 'styled-components';
+import BasicTemplate from 'templates/BasicTemplate';
 
 const Root = () => (
-  <div className="App">
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      <div className="container">
-        <header>
-          <MenuBar>
-            <ImageItem src={logo} alt="" />
-            <nav>
-              <ListItem>o mnie</ListItem>
-              <ListItem>szydelko</ListItem>
-              <ListItem>druty</ListItem>
-              <ListItem>projecty</ListItem>
-              <ListItem>kontakt</ListItem>
-            </nav>
-          </MenuBar>
-          <Heading> magia szydełka</Heading>
-        </header>
-        <main>
-          <section>
-            <Card />
-          </section>
-          <section>
-            <Card />
-          </section>
-          <section>
-            <Card />
-          </section>
-          <section>
-            <Card />
-          </section>
-        </main>
-        <footer>
-          <div>
-            <p>sociale: </p>
-            <ul>
-              <li>Insta</li>
-              <li>Facebook</li>
-            </ul>
-          </div>
-          <div>
-            <p>magiczna-petelka.io</p>
-            <p>powered by Pepe-dev &copy;</p>
-          </div>
-        </footer>
-      </div>
-    </ThemeProvider>
-  </div>
+  <BasicTemplate>
+    <>
+      <header>
+        <MenuBar>
+          <ImageItem src={logo} alt="" />
+          <nav>
+            <ListItem>o mnie</ListItem>
+            <ListItem>szydelko</ListItem>
+            <ListItem>druty</ListItem>
+            <ListItem>projecty</ListItem>
+            <ListItem>kontakt</ListItem>
+          </nav>
+        </MenuBar>
+        <Heading> magia szydełka</Heading>
+      </header>
+      <main>
+        <section>
+          <Card />
+        </section>
+        <section>
+          <Card />
+        </section>
+        <section>
+          <Card />
+        </section>
+        <section>
+          <Card />
+        </section>
+      </main>
+      <footer>
+        <div>
+          <p>sociale: </p>
+          <ul>
+            <li>Insta</li>
+            <li>Facebook</li>
+          </ul>
+        </div>
+        <div>
+          <p>magiczna-petelka.io</p>
+          <p>powered by Pepe-dev &copy;</p>
+        </div>
+      </footer>
+    </>
+  </BasicTemplate>
 );
 
 export default Root;
