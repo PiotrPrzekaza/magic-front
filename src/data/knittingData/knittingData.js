be7cb98f2a4e8227bbda091d545b7deb2cc4205b
Piotr Przekaza <piotr.przekaza@hotmail.com>
2020-12-10 19:15:03 +0100

    added some stock data of products

---
+++ src/data/knittingData/knittingData.js
@@ -0,0 +1,44 @@
+const knittingData = [
+  {
+    title: 'Opaska',
+    price: '120 zł',
+    desc: ' Cienka opaska wsam raz na jesienne wietrzne dni',
+    color: 'Różna',
+    imageUrl: 'https://picsum.photos/200/300',
+    createdTime: '10 dni',
+  },
+  {
+    title: 'Czapka',
+    price: '40 zł',
+    desc: ' Cienka opaska wsam raz na jesienne wietrzne dni',
+    color: 'Różna',
+    imageUrl: 'https://picsum.photos/200/300',
+    createdTime: '10 dni',
+  },
+  {
+    title: 'Rękawiczki',
+    price: '30 zł',
+    desc: ' Cienka opaska wsam raz na jesienne wietrzne dni',
+    color: 'Różna',
+    imageUrl: 'https://picsum.photos/200/300',
+    createdTime: '6 dni',
+  },
+  {
+    title: 'Szalik',
+    price: '25 zł',
+    desc: ' Cienka opaska wsam raz na jesienne wietrzne dni',
+    color: 'Różna',
+    imageUrl: 'https://picsum.photos/200/300',
+    createdTime: '5 dni',
+  },
+  {
+    title: 'Sweter',
+    price: '220 zł',
+    desc: ' Cienka opaska wsam raz na jesienne wietrzne dni',
+    color: 'Różna',
+    imageUrl: 'https://picsum.photos/200/300',
+    createdTime: '15 dni',
+  },
+];
+
+export default knittingData;
---
+++ src/data/productsData/productsData.js
@@ -0,0 +1,44 @@
+const productsData = [
+  {
+    title: 'Kocyk ',
+    price: '220 zł',
+    desc: 'Kocyk kolorowy dla maluszka',
+    color: 'mieszane',
+    imageUrl: '/assets/images/kocyk.jpg',
+    createdTime: '10 dni',
+  },
+  {
+    title: 'Plecak',
+    price: '40 zł',
+    desc: ' Plecak na wakacyjne wypady',
+    color: 'Niebieski',
+    imageUrl: '/assets/images/plecak.jpg',
+    createdTime: '10 dni',
+  },
+  {
+    title: 'Maskotka',
+    price: '30 zł',
+    desc: ' Kolorowy jednorożec - dziewczynka',
+    color: 'Róż',
+    imageUrl: '/assets/images/jednorozec.jpg',
+    createdTime: '6 dni',
+  },
+  {
+    title: 'Torebka',
+    price: '225 zł',
+    desc: ' Różowa torebka stylowa',
+    color: 'Róż',
+    imageUrl: '/assets/images/torebka-roz.jpg',
+    createdTime: '5 dni',
+  },
+  {
+    title: 'Kocyk - Maskotka',
+    price: '220 zł',
+    desc: 'Kocyk dla maluszka',
+    color: 'Róż',
+    imageUrl: '/assets/images/kocyknos.jpg',
+    createdTime: '7 dni',
+  },
+];
+
+export default productsData;
